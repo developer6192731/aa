ff8dcf5a48d9d1fee25c0b8569f2a7013b1e7be8
developer6192731 <99138666+developer6192731@users.noreply.github.com>
2022-02-10 23:06:13 +0600

    Update a.py

--- a.py
+++ a.py
@@ -1,14 +1,16 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 root.title("Text Maker")
 
 def cl():
+    txt = a.get(1.0, "end-1c")
     with open("file.txt", 'w') as f:
-        f.write()
+        f.write(txt)
 
 a = tk.Text(root)
-b = tk.Button(root, text='Make', command=cl)
+b = ttk.Button(root, text='Make', command=cl)
 a.pack()
 b.pack()
 
